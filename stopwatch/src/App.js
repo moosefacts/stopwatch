0e1365ef10d2debc9ee8ffbcfb6e4f0b8cd3ed2d
Chris <116923904+moosefacts@users.noreply.github.com>
2023-06-06 18:44:23 -0700

    first commit

---
+++ stopwatch/src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className='flex'>
+      <header className='flex mx-auto justify-center'>
+        <h1 className='flex justify-center place-items-center'>Stopwatch</h1>
+      </header>
+      <body>
+        <div>
+
+        </div>
+        <div>
+          <button className=''>
+            Start
+          </button>
+        </div>
+      </body>
+    </div>
+  );
+}
+
+export default App;
